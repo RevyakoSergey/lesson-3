12cfb5435268c9db91d85e7aa11b486610c928b9
sergey <grani2010@yandex.ru>
2022-10-09 13:14:56 +0300

    lesson 3

---
+++ lesson 3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+/* Console.Write("Введите пятизначное число:");
+string ?n= Console.ReadLine();
+
+if(n[0]==n[4]||n[1]==n[3])
+{
+   Console.Write("ваше число палиндром"); 
+}
+else Console.Write("ваше число не палиндром");
+
+
+Console.Write("Введите координату x1:");
+double x1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1:");
+double y1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1:");
+double z1=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x2:");
+double x2=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y2:");
+double y2=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2:");
+double z2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Round(Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2)),2));
+
+
+Console.Write("Введите число:");
+int n=Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+ Console.Write(i*i*i+" ");*/
+
